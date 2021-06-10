6142e983449efa28ab6684e4d9686b062716450b
Krzykoz <K.Krzykoz@Gmail.com>
2021-06-10 17:00:57 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Projekt_PK.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projekt_PK.dir/main.cpp.o"
+  "Projekt_PK"
+  "Projekt_PK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projekt_PK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
