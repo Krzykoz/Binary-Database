03e7660bfff5cfb0bf01594d52dd3795ebe103bd
Krzykoz <K.Krzykoz@Gmail.com>
2021-06-18 16:32:05 +0200

    file w/r

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 #include "pch.h"
 
 int main() {
-    Car* car = new Car("Tesla", "Model S", 2021, 1020.0f, "Sedan", "Solar Red", 5, 3);
+    Car car("Tesla", "Model S", 2021, 1020.0f, "Sedan", "Solar Red", 5, 3);
 
     Bike* bike = new Bike("Korss", "E-Bike MTB", 2020, 0.3f, 27, "Aluminium");
 
     Other* other = new Other("Xiaomi", "Mi Electric Scooter", 2018, 0.05f, 20, 5200);
 
-    car->describe();
+    car.describe();
     bike->describe();
     other->describe();
 
