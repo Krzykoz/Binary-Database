7fb4e707ae9cdb15b79eb61baa292c9e920a8b9f
Krzykoz <K.Krzykoz@Gmail.com>
2021-06-18 16:29:22 +0200

    file w/r

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "pch.h"
 
 int main() {
@@ -11,5 +12,31 @@ int main() {
     car->describe();
     bike->describe();
     other->describe();
+
+    std::fstream file;
+    file.open("records.dat", std::ios::out | std::ios::binary);
+
+    if(file.is_open()){
+        file.write(reinterpret_cast<char*>(&car), sizeof(Car));
+        file.close();
+    }else{
+        std::cout << "ERROR!";
+    }
+
+   std::cout << "\n" << sizeof(Car) << "\n";
+
+   Car read;
+
+   file.open("records.dat", std::ios::in | std::ios::binary);
+
+    if(file.is_open()){
+        file.read(reinterpret_cast<char*>(&read), sizeof(Car));
+        file.close();
+    }else{
+        std::cout << "ERROR!";
+    }
+
+    read.describe();
+
     return 0;
 }
