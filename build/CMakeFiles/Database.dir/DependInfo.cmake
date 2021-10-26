d16608e72da222510aeb2cd50428302c8604b92c
Krzykoz <K.Krzykoz@Gmail.com>
2021-10-26 23:02:22 +0200

    name changes

---
+++ build/CMakeFiles/Database.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/krzykoz/Developer/Binary-Database/Bike.cpp" "CMakeFiles/Database.dir/Bike.cpp.o" "gcc" "CMakeFiles/Database.dir/Bike.cpp.o.d"
+  "/Users/krzykoz/Developer/Binary-Database/Car.cpp" "CMakeFiles/Database.dir/Car.cpp.o" "gcc" "CMakeFiles/Database.dir/Car.cpp.o.d"
+  "/Users/krzykoz/Developer/Binary-Database/Database.cpp" "CMakeFiles/Database.dir/Database.cpp.o" "gcc" "CMakeFiles/Database.dir/Database.cpp.o.d"
+  "/Users/krzykoz/Developer/Binary-Database/Misc.cpp" "CMakeFiles/Database.dir/Misc.cpp.o" "gcc" "CMakeFiles/Database.dir/Misc.cpp.o.d"
+  "/Users/krzykoz/Developer/Binary-Database/Vehicle.cpp" "CMakeFiles/Database.dir/Vehicle.cpp.o" "gcc" "CMakeFiles/Database.dir/Vehicle.cpp.o.d"
+  "/Users/krzykoz/Developer/Binary-Database/main.cpp" "CMakeFiles/Database.dir/main.cpp.o" "gcc" "CMakeFiles/Database.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Database.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Database.
+# This may be replaced when dependencies are built.
